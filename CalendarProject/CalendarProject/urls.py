f7474d008b31e956e4f463bcf5e8f74787dbb7a3
Sagun Raj Lage <sagsraj@yahoo.com>
2018-06-07 15:15:10 +0545

    Added details page

--- CalendarProject/CalendarProject/urls.py
+++ CalendarProject/CalendarProject/urls.py
@@ -19,5 +19,6 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('entry/<int:pk>', views.details, name="details"),
 ]
